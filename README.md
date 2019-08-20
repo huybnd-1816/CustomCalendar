00ebfdfba07cb6ecc462605035737f0afe62dc74
Nguyen Duc Huy B <nguyen.duc.huyb@sun-asterisk.com>
2019-08-20 11:00:02 +0700

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CustomCalendar
+# Swift-Calendar
+
+![](https://raw.githubusercontent.com/samirpdl/Swift-Calendar/master/demo.png)
+
+Document: https://iosexample.com/a-custom-calendar-made-in-swift/
---
+++ SwiftCalendar/SwiftCalendar/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SwiftCalendar
+//
+//  Created by nguyen.duc.huyb on 8/19/19.
+//  Copyright © 2019 nguyen.duc.huyb. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
